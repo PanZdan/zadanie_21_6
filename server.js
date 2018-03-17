dcb8fcd1084df6f1c116d1515fb430608045deb4
Konrad Zdanowski <konrad-zdanowski@wp.pl>
2018-03-17 23:19:41 +0100

    Add app to the server

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://PANZDAN:123Panzdan@ds115579.mlab.com:15579/nodeappdatabase', {
-    useMongoClient: true
-});
+mongoose.connect('mongodb://PANZDAN:123Panzdan@ds115579.mlab.com:15579/nodeappdatabase');
 
 //new user Schema
 const userSchema = new Schema({
